d1e3ad3d2aed2a7cb4be20ea7182073846ac154a
channn3 <channn3@fpt.edu.vn>
2021-07-07 20:03:14 +0700

    next

--- Android_UI/Miminal/app/src/main/java/com/example/myapplication/Activities/MainActivity.java
+++ Android_UI/Miminal/app/src/main/java/com/example/myapplication/Activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,20 +12,31 @@ import android.widget.Toast;
 
 import com.example.myapplication.DAO.UsersDAO;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText username, password;
     private Button buttonLogin;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         // kiem tra login, neu da dang nhap >>> chay qua man hinh welcome
         // nguoc lai cho dang nhap
-        checkLoginStatus();
+//        checkLoginStatus();
 
         username = (TextInputEditText) findViewById(R.id.editTextUsername);
         password = (TextInputEditText) findViewById(R.id.editTextPassword);
@@ -35,26 +47,48 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String _username = username.getText().toString();
                 String _pass = password.getText().toString();
-                UsersDAO dao = new UsersDAO(MainActivity.this);
-                boolean isLoggedIn = dao.login(_username, _pass);
-                if (isLoggedIn == true){
-                    // luu trang thai dang nhap
-                    SharedPreferences.Editor editor =
-                            getSharedPreferences("login_status",MODE_PRIVATE).edit();
-                    editor.putBoolean("isLoggedIn", true);
-                    editor.apply();
-                    // chuyen man hinh
-                    Intent intent = new Intent(MainActivity.this, TaskActivity.class);
-                    startActivity(intent);
-                    finish();
-                    return;
-                }
-                else {
-                    Toast.makeText(MainActivity.this,
-                            "Login failed", Toast.LENGTH_LONG).show();
-                }
+                // dung FB
+                signIn(_username, _pass);
+
+//
+//                UsersDAO dao = new UsersDAO(MainActivity.this);
+//                boolean isLoggedIn = dao.login(_username, _pass);
+//                if (isLoggedIn == true){
+//                    // luu trang thai dang nhap
+//                    SharedPreferences.Editor editor =
+//                            getSharedPreferences("login_status",MODE_PRIVATE).edit();
+//                    editor.putBoolean("isLoggedIn", true);
+//                    editor.apply();
+//                    // chuyen man hinh
+//                    Intent intent = new Intent(MainActivity.this, TaskActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                    return;
+//                }
+//                else {
+//                    Toast.makeText(MainActivity.this,
+//                            "Login failed", Toast.LENGTH_LONG).show();
+//                }
             }
         });
+    }
+
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent intent = new Intent(MainActivity.this, TaskActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                        }
+                    }
+                });
     }
 
     private void checkLoginStatus(){
@@ -65,6 +99,21 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(), TaskActivity.class));
             finish();
             return;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(MainActivity.this, TaskActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+
         }
     }
 }
